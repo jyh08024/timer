e799e4a95a7d091b2d518a27a5918a7c349c2603
jyh08024 <jyh08024@gmail.com>
2022-09-15 21:55:16 +0900

    timer function add
    timer function add

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,8 @@
+// 디버깅
 const dd = console.log;
+
+// 패드
+const pad = (num) => num < 10 ? "0" + num : num;
 
 const App = {
   init() {
@@ -13,18 +17,39 @@ const App = {
 
 const Timer = {
   init() {
+    const today = new Date();
 
+    const year = today.getFullYear();
+    const month = pad(today.getMonth() + 1);
+    const day = pad(today.getDay());
+
+    document.querySelector(".today").innerHTML = `${year}년 ${month}월 ${day}일`;
+
+    setInterval(() => {
+      const t = new Date();
+      const h = pad(12 - (24 - t.getHours()));
+      const m = pad(t.getMinutes());
+      const s = pad(t.getSeconds());
+      
+      Timer.nowTimer(h, m, s);
+    }, 1000);
+  },
+
+  nowTimer(h, m, s) {
+    document.querySelector(".now_time").innerHTML = Timer.nowTimerHtml(h, m, s);
   },
 
   nowTimerHtml(hour, min, sec) {
     return `
-        <h1 class="now_time"><span class="hour">00</span>:<span class="min">00</span>:<span class="sec">00</span></h1>
+      <h1 class="hour">${hour}</h1>
+      <h1 class="min">${min}</h1>
+      <h1 class="sec">${sec}</h1>
       `
   },
 
   leftTimerHtml(hour, min, sec) {
     return `
-        <h1 class="left_time"><span class="hour">00</span>:<span class="min">00</span>:<span class="sec">00</span></h1>
+        <h1 class="left_time time"><span class="hour">00</span>:<span class="min">00</span>:<span class="sec">00</span></h1>
       `
   }
 }
